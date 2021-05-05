ac367e8843384a1cbf085558f4a82602688d1d8d
Alex Albaran <albaranlex@icloud.com>
2021-05-05 18:14:50 +0200

    added script

---
+++ script/script.js
@@ -0,0 +1,28 @@
+//SELECTORS
+const userInput = document.querySelector(".amount");
+const currencyInput = document.querySelector(".end-currency-type");
+const endAmount = document.querySelector(".end-amount");
+const convertButton = document.querySelector("#convert");
+
+const currencyRates = {
+  usd: 1.2,
+  jpy: 131.13,
+  gbp: 0.86,
+  aud: 1.55,
+  cad: 1.47,
+  chf: 1.1,
+  cny: 7.77,
+  sek: 10.19,
+  nzd: 1.66,
+};
+
+//FUNCTIONS
+function convertCurrency() {
+  let amount = userInput.value;
+  let targetCurrency = currencyRates[currencyInput.value];
+  let convertedAmount = amount * targetCurrency;
+  endAmount.innerText = convertedAmount.toFixed(2);
+}
+
+//EVENT LISTENERS
+convertButton.addEventListener("click", convertCurrency);
